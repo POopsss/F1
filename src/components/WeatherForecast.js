d9856e53a673cf909f176c5e833864dbefb577ce
Your Name <you@example.com>
2023-10-02 20:12:53 +0300

    ''

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -6,37 +6,32 @@ import Cities from "./Cities"
 import "../styles/WeatherForecast.css"
 
 function WeatherForecast() {
-    const [tempMax, settempMax] = React.useState([]);
-    const [tempMin, settempMin] = React.useState([]);
-    const [tempTime, settempTime] = React.useState([]);
     const [city, setCity] = React.useState();
+    const [weatherData, setweatherData] = React.useState({});
 
     function getLocation(){
         const geocodingURl = 'https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name=';
         setCity(Cities[document.getElementById("weather-forecast-select-city").value]);
+        axios.get(geocodingURl + document.getElementById("weather-forecast-select-city").value).then(res => {
+            getWeather(res.data.results[0]);
+        });
+    };
+
+    function getWeather(res){
         const start_date = new Date();
         const end_date = new Date();
         if (document.getElementById("weather-forecast-checkbox").checked) {
             end_date.setDate(end_date.getDate() + 4);
         };
-
-        axios.get(geocodingURl + document.getElementById("weather-forecast-select-city").value).then(res => {
-            res = res.data.results[0];
-            const latitude = res.latitude;
-            const longitude = res.longitude;
-            const weatherURL = `https://api.open-meteo.com/v1/forecast?daily=temperature_2m_max,temperature_2m_min&timezone=GMT&latitude=${latitude}&longitude=${longitude}&start_date=${start_date.toISOString().slice(0, 10)}&end_date=${end_date.toISOString().slice(0, 10)}`;
-            
-            axios.get(weatherURL).then(res => {
-                res = res.data.daily;
-                settempMax(res.temperature_2m_max);
-                settempMin(res.temperature_2m_min);
-                settempTime(res.time);
-            });
+        const weatherURL = `https://api.open-meteo.com/v1/forecast?daily=temperature_2m_max,temperature_2m_min&timezone=GMT&latitude=${res.latitude}&longitude=${res.longitude}&start_date=${start_date.toISOString().slice(0, 10)}&end_date=${end_date.toISOString().slice(0, 10)}`;
+        axios.get(weatherURL).then(res => {
+            setweatherData(res.data.daily);
         });
+
     };
 
     return (
-        <div>
+        <>
             <div className="weather-forecast-input">
                 <div>
                     <select id="weather-forecast-select-city">
@@ -45,16 +40,16 @@ function WeatherForecast() {
                 </div>
                 <div>
                     <input className="weather-forecast-checkbox" id="weather-forecast-checkbox" type="checkbox" />
-                    <label for="weather-forecast-checkbox" >На ближайшие 5 дней.</label>
+                    <label htmlFor="weather-forecast-checkbox" >На ближайшие 5 дней.</label>
                 </div>
                 <div>
                     <button id="weather-forecast-button" onClick={getLocation}>КлиК</button>
                 </div>
             </div>
             <div><h1>{ city }</h1></div>
-            <WeatherForecastDisplay tempMax={tempMax} tempMin={tempMin} tempTime={tempTime}/>
-        </div>
-        )
+            { Object.entries(weatherData).length > 0 ? <WeatherForecastDisplay weatherData={weatherData}/> : ""}
+        </>
+        );
 };
 
 export default WeatherForecast;
--- src/components/WeatherForecastDisplay.js
+++ src/components/WeatherForecastDisplay.js
@@ -1,51 +1,94 @@
-    import React from "react";
-    import "../styles/WeatherForecastDisplay.css"
+import React from "react";
+import "../styles/WeatherForecastDisplay.css"
 
-    function WeatherForecastDisplay(prop) {
-        const monthNames = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня","Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
-        const xdot = [45, 245, 445, 645, 845];
-        const ydot = [10, 50, 90, 130, 170, 210];
-        const tempMark = [25, 20, 15, 10, 5, 0];
+function WeatherForecastDisplay(prop) {
+    const monthNames = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня","Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
 
-        const ydotMax = prop.tempMax.map(x => 210 - x * 8);
-        const ydotMin = prop.tempMin.map(x => 210 - x * 8);
-        const time = prop.tempTime.map(time => 
-            `${new Date(time).getDate()} ${monthNames[new Date(time).getMonth()]}`
-            );
-        const maxline = xdot.map((x, i) => [x, ydotMax[i]]).join(' ');
-        const minline = xdot.map((x, i) => [x, ydotMin[i]]).join(' ');
-        
-        function dispayDescription(e) {
-            const id = e.target.dataset.id;
-            const div = document.getElementById('weather-forecast-dispay-description')
-            div.innerHTML = `<h3>${time[id]}</h3><p>Суточная температура: ${prop.tempMin[id]} - ${prop.tempMax[id]} °C</p>`
-        }
+    const xdot = [45, 245, 445, 645, 845];
+    const ydot = [10, 50, 90, 130, 170, 210];
+    const tempMark = [25, 20, 15, 10, 5, 0];
 
-        return (
-        <div className="weather-forecast-dispay">
-            { time.length > 1 ?
+    const temp = {
+        "max": prop.weatherData.temperature_2m_max,
+        "min": prop.weatherData.temperature_2m_min
+    };
+    const ydotMax = temp.max.map(x => 210 - x * 8);
+    const ydotMin = temp.min.map(x => 210 - x * 8);
+    const time = prop.weatherData.time.map(time => 
+        `${new Date(time).getDate()} ${monthNames[new Date(time).getMonth()]}`
+        );
+    const maxline = xdot.map((x, i) => [x, ydotMax[i]]).join(' ');
+    const minline = xdot.map((x, i) => [x, ydotMin[i]]).join(' ');
+    
+    function dispayDescription(e) {
+        const id = e.target.dataset.id;
+        const div = document.getElementById('weather-forecast-dispay-description');
+        div.innerHTML = `<h3>${time[id]}</h3><p>Суточная температура: ${prop.weatherData.temperature_2m_min[id]} - ${prop.weatherData.temperature_2m_max[id]} °C</p>`;
+    };
+
+    return (
+    <div className="weather-forecast-dispay">
+        { time.length > 1 ?
             <svg width="900" height="240" xmlns="http://www.w3.org/2000/svg">
 
-                <text x="10" y="15" transform="translate(0) rotate(270 60 60)">&deg;C</text>    {/* °C */}
-                <polyline className="svg-abscissa-line" points={`${xdot[0] - 10},${ydot[ydot.length - 1]} ${xdot[xdot.length - 1] + 10},${ydot[ydot.length - 1]}`} />    {/* Линия оси абсцисс */}
-                { ydot.map((y, i) => <polyline className="svg-celsius-line" points={`${xdot[0] - 10},${y} ${xdot[xdot.length - 1] + 10},${y}`} key={y}/>) }    {/* Линии метки градусов */}
-                { time.map((time, i) => <text x={xdot[i] - 35} y={ydot[ydot.length - 1] + 25} key={time}>{time}</text>) }    {/* Обозначение меток дат */}
-                { xdot.map((x, i) => <polyline className="svg-date-mark" points={`${x},${ydot[ydot.length - 1]} ${x},${ydot[ydot.length - 1] + 8}`} key={time[i]}/>) }    {/* Метки дат */}
-                { tempMark.map((mark, i) => <text x={xdot[0] - 30} y={ydot[i] + 5} key={mark}>{mark}</text>)}    {/* Обозначения меток температуры */}
-                { ydot.map((y, i) => <circle className="svg-celsius-mark" cx={xdot[0] - 10} cy={y} r="2" key={y}/>)}    {/* Метки температуры */}
+                {/* °C */}
+                <text x="10" y="15" transform="translate(0) rotate(270 60 60)">&deg;C</text>
+                {/* Линия оси абсцисс */}
+                <polyline className="svg-abscissa-line" points={`${xdot[0] - 10},${ydot[ydot.length - 1]} ${xdot[xdot.length - 1] + 10},${ydot[ydot.length - 1]}`} />
+                {/* Линии метки градусов */}
+                { ydot.map((y, i) => 
+                <polyline className="svg-celsius-line" points={`${xdot[0] - 10},${y} ${xdot[xdot.length - 1] + 10},${y}`} key={y}/>
+                )}
+                {/* Обозначение меток дат */}
+                { time.map((time, i) => 
+                <text x={xdot[i] - 35} y={ydot[ydot.length - 1] + 25} key={time}>{time}</text>
+                )}
+                {/* Метки дат */}
+                { xdot.map((x, i) => 
+                <polyline className="svg-date-mark" points={`${x},${ydot[ydot.length - 1]} ${x},${ydot[ydot.length - 1] + 8}`} key={time[i]}/>
+                )}
+                {/* Обозначения меток температуры */}
+                { tempMark.map((mark, i) => 
+                <text x={xdot[0] - 30} y={ydot[i] + 5} key={mark}>{mark}</text>
+                )}
+                {/* Метки температуры */}
+                { ydot.map((y, i) => 
+                <circle className="svg-celsius-mark" cx={xdot[0] - 10} cy={y} r="2" key={y}/>
+                )}
 
-                <polyline className="svg-temperature-max-line" points={maxline}/>    {/* Линия максимальной дневной температуры */}
-                { xdot.map((x, i) => <circle className="svg-temperature-mark" cx={x} cy={ydotMax[i]} r="5" data-id={i} key={time[i]} onMouseEnter={dispayDescription}/>)}    {/* Точки максимальной дневной температуры */}
+                {/* Линия максимальной дневной температуры */}
+                <polyline className="svg-temperature-max-line" points={maxline}/>
+                {/* Точки максимальной дневной температуры */}
+                { xdot.map((x, i) => 
+                <circle className="svg-temperature-mark" cx={x} cy={ydotMax[i]} r="5" data-id={i} key={time[i]} onMouseEnter={dispayDescription}/>
+                )}
 
-                <polyline className="svg-temperature-min-line" points={minline}/>    {/* Линия минимальной дневной температуры */}
-                { xdot.map((x, i) => <circle className="svg-temperature-mark" cx={x} cy={ydotMin[i]} r="5" data-id={i} key={time[i]} onMouseEnter={dispayDescription}/>)}    {/* Точки минимальной дневной температуры */}
+                {/* Линия минимальной дневной температуры */}
+                <polyline className="svg-temperature-min-line" points={minline}/>
+                {/* Точки минимальной дневной температуры */}
+                { xdot.map((x, i) => 
+                <circle className="svg-temperature-mark" cx={x} cy={ydotMin[i]} r="5" data-id={i} key={time[i]} onMouseEnter={dispayDescription}/>
+                )}    
 
             </svg>
-            : "" }
-            { time.length > 1 ? <div className="weather-forecast-dispay-description" id="weather-forecast-dispay-description"><h3>{time[0]}</h3><p>Суточная температура: {prop.tempMin[0]} - {prop.tempMax[0]} °C</p></div> : "" }
-            { time.length === 1 ? <div className="weather-forecast-dispay-description"><h3>{time[0]}</h3><p>Суточная температура: {prop.tempMin[0]} - {prop.tempMax[0]} °C</p></div> : "" }
-        </div>
-        )
-    }
+        : "" }
 
-    export default WeatherForecastDisplay
+        { time.length > 1 ? 
+            <div className="weather-forecast-dispay-description" id="weather-forecast-dispay-description">
+                <h3>{time[0]}</h3>
+                <p>Суточная температура: {temp.min[0]} - {temp.max[0]} °C</p>
+            </div>
+        : "" }
+
+        { time.length === 1 ? 
+            <div className="weather-forecast-dispay-description">
+                <h3>{time[0]}</h3>
+                <p>Суточная температура: {temp.min[0]} - {temp.max[0]} °C</p>
+            </div>
+        : "" }
+
+    </div>
+    );
+};
+
+export default WeatherForecastDisplay;
